d42f76f4693f761f61a76999dd2d3b1becc87412
Amine Mohyeddine <amohyeddine@smb.sqli.com>
2022-05-12 22:43:31 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({ origin: '*', credentials: true });
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
